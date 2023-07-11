eda96ebeffa00a2415cc00ce8ae6e9624584a015
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-07-11 13:21:59 +0300

    Add comment to custom permission in cinema/permissions.py

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -2,6 +2,11 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
+    """
+    The request is authenticated as an admin user,
+    or is a read-only for non-admin users request.
+    """
+
     def has_permission(self, request, view):
         return bool(
             (
