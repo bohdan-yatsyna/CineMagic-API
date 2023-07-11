149d2cfde07deadd5df3e368f5e3d5f7bab50d25
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-07-11 12:58:14 +0300

    Delete redundant AuthTokenSerializer & unused imports
    From user/serializers.py

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,4 @@
-from django.contrib.auth import get_user_model, authenticate
-from django.utils.translation import gettext as _
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 
@@ -8,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ("id", "email", "password", "is_staff")
         read_only_fields = ("is_staff",)
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+        extra_kwargs = {"password": {"write_only": True, "min_length": 8}}
 
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
@@ -23,30 +22,3 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
-
-
-class AuthTokenSerializer(serializers.Serializer):
-    email = serializers.EmailField(max_length=255)
-    password = serializers.CharField(max_length=128, write_only=True)
-    token = serializers.CharField(read_only=True)
-
-    def validate(self, attrs):
-        email = attrs.get("email")
-        password = attrs.get("password")
-
-        if email and password:
-            user = authenticate(
-                request=self.context.get("request"),
-                email=email,
-                password=password
-            )
-            if not user:
-                msg = _("Unable to log in with provided credentials.")
-                raise serializers.ValidationError(msg, code="authorization")
-
-        else:
-            msg = _("Must include 'email' and 'password'.")
-            raise serializers.ValidationError(msg, code="authorization")
-
-        attrs["user"] = user
-        return attrs
